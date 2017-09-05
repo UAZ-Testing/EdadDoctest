f76cf0b74d0363ff4afc75614daac9670d68e8a2
porfirioangel <porfirioads@gmail.com>
2017-09-05 10:31:43 -0500

    Se implementaron y pasaron todas las pruebas

--- edad.py
+++ edad.py
@@ -4,3 +4,14 @@ class Edad:
     def evaluar_edad(self, edad):
         if edad < 0:
             print('no existes')
+        elif edad < 13:
+            print('eres niño')
+        elif edad < 18:
+            print('eres adolescente')
+        elif edad < 65:
+            print('eres adulto')
+        elif edad < 120:
+            print('eres adulto mayor')
+        else:
+            print('eres Mumm-Ra')
+
